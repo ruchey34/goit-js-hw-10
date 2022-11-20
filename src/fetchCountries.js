adf259437490ce45bd94aaf7fe1e56f11e38f21a
ruchey34 <me.oleg1@gmail.com>
2022-11-20 16:06:31 +0200

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+import { Notify } from 'notiflix';
+// export {fetchCountries};
+
+export function fetchCountries(name){
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    .then((response) =>{
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+        return response.json();
+    }).catch(function (error) {
+        Notify.failure('Oops, there is no country with that name');
+        console.log('404:' + error);
+      });
+}
